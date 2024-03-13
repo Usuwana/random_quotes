c7396bcba794377aeb3e518c1f4fa759f5936856
Tatenda Usuwana Mapuranga <tatemapu@gmail.com>
2024-03-13 19:32:37 +1100

    .

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
-import dark from './/darky.png';
-import light from './/light.png';
+import dark from './/darker.png';
+import light from './/lighty.png';
 
 export const Home = () => {
     const [theme, setTheme] = useState('light');
@@ -18,8 +18,10 @@ export const Home = () => {
       }, [theme]);
     return (
         <div className={`App ${theme}`}>
-            <button onClick={toggleTheme}>{theme == 'light' ? <img src={light}/> : <img src={dark}/> }</button>
-          <h1>Hello, world!</h1>
+            <button onClick={toggleTheme} style={{position:'absolute', top: '1px', left:'0', backgroundColor: 'transparent', border: 'none', cursor: 'pointer'}}>{theme == 'light' ? <img src={dark} style={{width: '30px'}}/> : <img src={light} style={{width: '30px'}}/> }</button>
+          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+          <h1>[Movie quote here]</h1>
+          </div>
         </div>
       );
 }
